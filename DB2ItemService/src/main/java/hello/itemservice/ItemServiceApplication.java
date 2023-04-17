4d7b7ca2e4ae79ba79eb4554becd67a58d2de3a8
fearlessdeveloper <1dilumn0@gmail.com>
2023-04-17 14:11:35 +0900

    JPA/Spring Data JPA

--- DB2ItemService/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ DB2ItemService/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 //@Import(MemoryConfig.class)
 @Slf4j
-@Import(MyBatisConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
